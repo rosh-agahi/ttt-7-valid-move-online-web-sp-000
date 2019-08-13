cebf149d2672655c9c812a019d122923ee0f63f8
Roshanak Agahi <rosh.agahi@gmail.com>
2019-08-13 17:14:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ end
 
 def position_taken?(board,index)
   if 
-    (board[index]== " ") && (board[index] == "")
+    (board[index]== " ") || (board[index] == "") || (board[index] == nil)
     return false
   else
     (board[index] == "X") || (board[index] == "O")
