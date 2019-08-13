2bf19771e65e4bef89471f001cb30f07a1468a8c
Roshanak Agahi <rosh.agahi@gmail.com>
2019-08-13 17:18:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,4 @@
 # code your #valid_move? method here
 
-def valid_move?
-  if position_taken? == true
-    return false
-  else position_taken? == false
-    return true
-end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
